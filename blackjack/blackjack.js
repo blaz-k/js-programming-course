4fc220af12e7ae7d205e0ade27e612586a17a9e1
blaz <blazyy@gmail.com>
2021-08-25 07:44:58 +0200

    no message

---
+++ blackjack/blackjack.js
@@ -0,0 +1,22 @@
+
+/*
+let firstCard = 19
+let secondCard = 2
+
+let sum = firstCard + secondCard;
+
+if (sum < 21){
+    console.log("Do you want to draw a new card?")
+} else if (sum === 21){
+    console.log("You've got Blackjack!!")
+}else if (sum > 21){
+    console.log("You are out of the game!")
+}*/
+
+let age = 21
+
+if (age < 21){
+    console.log("You cannot enter the club")
+}else if (age >= 21){
+    console.log("you can enter the club")
+}
