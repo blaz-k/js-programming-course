942258f2a233487c683f5f6a9fdaefc91ff04f45
blaz <blazyy@gmail.com>
2021-08-26 13:48:47 +0200

    for statment, you can add more cards

--- blackjack/blackjack.js
+++ blackjack/blackjack.js
@@ -1,22 +1,58 @@
 
-/*
-let firstCard = 19
-let secondCard = 2
+console.log("blackjack")
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+
+let cards = [firstCard, secondCard]
 
 let sum = firstCard + secondCard;
 
-if (sum < 21){
-    console.log("Do you want to draw a new card?")
-} else if (sum === 21){
-    console.log("You've got Blackjack!!")
-}else if (sum > 21){
-    console.log("You are out of the game!")
-}*/
+let hasBlackjack = false;
+let isAlive = true;
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-let age = 21
-
-if (age < 21){
-    console.log("You cannot enter the club")
-}else if (age >= 21){
-    console.log("you can enter the club")
+function getRandomCard() {
+    return 5
 }
+
+
+function startGame() {
+    renderGame();
+}
+
+
+function renderGame() {
+    sumEl.textContent = "Sum: " + sum;
+    cardsEl.textContent = "Cards: "
+    for ( let i = 0; i < cards.length; i++ ) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+
+    if (sum <= 20){
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21){
+        hasBlackjack = true;
+        message = "You've got Blackjack!!";
+
+    }else {
+        isAlive = false;
+        message = "You are out of the game!";
+    }
+    messageEl.textContent = message;
+
+}
+
+
+
+function newCard() {
+    let thirdCard = getRandomCard();
+    sum += thirdCard;
+    cards.push(thirdCard);
+    console.log(cards)
+    renderGame();
+}
+
